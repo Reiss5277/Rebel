b3785cb081647e80975c97b89303ad25a8617609
Reiss5277 <encoree219999@live.com>
2024-07-06 20:56:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    window.location.href = 'success.html';
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    window.location.href = 'success.html';
+});
